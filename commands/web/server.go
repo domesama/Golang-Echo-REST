37083b87ce7f6287bff9a3524bcc1dd9d0f463af
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2021-03-21 17:48:19 +0700

    [Add] env & pre-initialization

--- commands/web/server.go
+++ commands/web/server.go
@@ -2,22 +2,29 @@ package web
 
 import (
 	"fmt"
+	"github.com/domesama/Golang-Echo-REST/configs"
 	"github.com/domesama/Golang-Echo-REST/routes"
 	"github.com/domesama/Golang-Echo-REST/validators"
 	"github.com/go-playground/validator/v10"
+	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
-	"os"
 )
 
 var e = echo.New()
+var config = &configs.ConfigDatabase{}
+
+func init()  {
+	err := cleanenv.ReadEnv(config)
+	if err != nil{
+		e.Logger.Fatal(err)
+	}
+	fmt.Println(config)
+}
 
 //Start starts the application
 func Start()  {
+	//port := os.Getenv("DEV_PORT")
 
-	port := os.Getenv("DEV_PORT")
-	if port == ""{
-		port = "8080"
-	}
 
 	//Add Custom Validator
 	e.Validator = &validators.CustomValidator{ Validator: validator.New()}
@@ -25,8 +32,8 @@ func Start()  {
 	//Routes
 	routes.GetDefaultRoutes(e)
 
-	e.Logger.Print(fmt.Sprintf("Started server on http://localhost:%s", port))
-	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
+	e.Logger.Print(fmt.Sprintf("Started server on http://localhost:%s", config.Port))
+	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", config.Port)))
 }
 
 
---
+++ configs/database.go
@@ -0,0 +1,9 @@
+package configs
+
+type ConfigDatabase struct {
+	AppName string `env:"APP_NAME" env-default:"OwO"`
+	AppEnv string `env:"APP_ENV" env-default:"DEV"`
+	Port string `env:"MY_APP_PORT" env-default:"8080"`
+	Host string `env:"HOST" env-default:"localhost"`
+	LogLevel string `env:"LOG_LEVEL" env-default:"ERROR"`
+}
